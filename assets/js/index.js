94d2a12d359ff65fbe42348c5930b64ae731dfdd
Tarek <tarek.elouatar@gmail.com>
2017-11-27 10:24:03 +0100

    cv

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$('.croix1').click(function(){
+  $('.block1').hide();
+});
+$('.croix2').click(function(){
+  $('.block2').hide();
+});
+$('.croix3').click(function(){
+  $('.block3').hide();
+});
+$('.croix4').click(function(){
+  $('.block4').hide();
+});
+$('.croix5').click(function(){
+  $('.block5').hide();
+});
+$('.agrandir1').click(function(){
+  $('.block1').css({
+    height: '500px'
+  });
+});
